7c9116fb78e74adcfb337506af5cd3b171515555
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-05-09 13:31:32 -0600

    feat: Add API endpoint for syncing Clerk user to Prisma DB

---
+++ src/app/api/users/sync-clerk-user/route.ts
@@ -0,0 +1,36 @@
+import { auth } from "@clerk/nextjs/server";
+import prisma from "@/lib/db";
+import { NotAuthorizedError, ServerError } from "@api/_services/errors";
+import { SuccessCreate, SuccessUpdate } from "@api/_services/successfulResponses";
+
+export async function POST(req: Request) {
+    const { userId } = await auth();
+
+    if (!userId) {
+        return NotAuthorizedError();
+    }
+
+    try {
+        const { email, username } = await req.json();
+
+        const user = await prisma.user.upsert({
+            where: { id: userId },
+            update: {
+                email: email,
+                username: username,
+            },
+            create: {
+                id: userId,
+                email: email,
+                username: username,
+            },
+        });
+
+
+        return SuccessUpdate(user);
+
+    } catch (error) {
+        console.error("Error syncing Clerk user to Prisma DB:", error);
+        return ServerError();
+    }
+}
