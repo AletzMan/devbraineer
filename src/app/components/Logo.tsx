ab1cf0f329b0e08198c1a923e03f07db611cf25c
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-05-17 10:43:20 -0600

    feat: Add Logo component for displaying company logo

---
+++ src/app/components/Logo.tsx
@@ -0,0 +1,46 @@
+export function Logo(props: React.SVGProps<SVGSVGElement>) {
+    return (
+        <svg
+            viewBox="0 0 83.6 77.58"
+            {...props}
+        >
+            <path
+                d="M71.65 21.89a11.94 11.94 0 0 0-18.13-10.21 11.94 11.94 0 0 0-23.45 0"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="4px"
+                fill="none"
+            />
+            <path
+                d="M71.65 21.89a9.95 9.95 0 0 1 0 19.9h-59.7a9.95 9.95 0 0 1 0-19.9 11.93 11.93 0 0 1 18.12-10.21"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="4px"
+                fill="none"
+            />
+            <path
+                d="M71.65 21.89a9.94 9.94 0 0 0-8 4M25.39 9.95a11.92 11.92 0 0 1 7.12 3.73"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="4px"
+                fill="none"
+            />
+            <path
+                d="M38.08 68.78a25.66 25.66 0 0 0 3.72.27 27.85 27.85 0 0 0 3.76-.27l6.34 8.09a1.91 1.91 0 0 0 2.45.44l13.22-7.65a1.94 1.94 0 0 0 .82-2.35l-3.82-9.51a28.81 28.81 0 0 0 3.77-6.41l10.1-1.42a2 2 0 0 0 1.58-1.92v-7.66H55.09a13.3 13.3 0 1 1-26.59 0H3.57v7.65a2 2 0 0 0 1.64 1.92l10.07 1.43a29.36 29.36 0 0 0 3.8 6.4l-3.8 9.52a1.93 1.93 0 0 0 .82 2.35l13.18 7.65a1.92 1.92 0 0 0 2.46-.44Z"
+                fill="currentColor"
+            />
+            <path
+                d="M33.25 17.84h33.5v33.51h-33.5Z"
+                fill="none"
+                transform="translate(-8.72 -9.61)"
+            />
+            <path
+                d="M44.75 12.62a1.4 1.4 0 0 1 1 1.72l-6.22 22.05a1.4 1.4 0 1 1-2.69-.75l6.19-22a1.39 1.39 0 0 1 1.72-1m-10.16 5.43a1.41 1.41 0 0 1 0 2L29.65 25l4.94 4.94a1.414 1.414 0 0 1-2 2l-5.92-5.92a1.38 1.38 0 0 1 0-2l5.92-5.93a1.41 1.41 0 0 1 2 0m13.38 2a1.4 1.4 0 0 1 1.94-2l5.92 5.93a1.38 1.38 0 0 1 0 2l-5.88 5.87a1.414 1.414 0 0 1-2-2l4.94-4.94Z"
+                fill="currentColor"
+            />
+        </svg>
+    )
+}
