33f88cc9b664f56d50bb7bc78e29093aca2753f5
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-05-17 17:50:42 -0600

    feat: Add breadcrumb navigation to Header component

--- src/app/platform/componentes/Header.tsx
+++ src/app/platform/componentes/Header.tsx
@@ -1,20 +1,27 @@
 "use client"
+import { UserButton } from "@clerk/nextjs"
+import { dark } from "@clerk/themes"
 import { useState } from "react"
+import { usePathname } from "next/navigation"
+import { menuData } from "../constants"
 
 export default function Header() {
     const [theme, setTheme] = useState("light")
+    const path = usePathname()
+    console.log(path)
     return (
-        <header className="  flex items-center justify-between mb-6 h-16 w-full bg-(--color-base-100) border-b-1 border-(--color-gray-700)">
-            <div className="flex items-center gap-2">
-                <select
-                    value={theme}
-                    onChange={(e) => setTheme(e.target.value)}
-                    className="select select-bordered w-full max-w-xs"
-                >
-                    <option value="light">Light</option>
-                    <option value="dark">Dark</option>
-                </select>
+        <header className=" flex items-center justify-between h-16 w-full bg-(--color-base-100) border-b-1 border-(--color-gray-700) z-50 px-2">
+            <div className="breadcrumbs text-sm">
+                <ul>
+                    {path.split("/").map((item, index) => index > 0 && (
+                        <li key={index} className="capitalize text-yellow-400">
+                            {index === path.split("/").length - 1 && item}
+                            {index > 0 && index < path.split("/").length - 1 && <a className="text-gray-400" href={`/${item}`}>{item}</a>}
+                        </li>
+                    ))}
+                </ul>
             </div>
+            <UserButton appearance={{ baseTheme: dark }} userProfileMode="navigation" userProfileUrl="/profile" />
         </header>
     )
 }
