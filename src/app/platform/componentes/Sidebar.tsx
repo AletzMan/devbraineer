6b9de6dacd7e827bfb806a06ff9a58f726b49986
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-05-17 10:43:31 -0600

    style: Refactor Sidebar component for improved structure and readability

--- src/app/platform/componentes/Sidebar.tsx
+++ src/app/platform/componentes/Sidebar.tsx
@@ -1,30 +1,22 @@
 "use client"
-import Link from "next/link"
 import { Code2 } from "lucide-react"
 import { usePathname } from "next/navigation"
-import { useState } from "react"
 import { MenuItem } from "./MenuItem"
 import { menuData } from "../constants"
+import { Logo } from "@/app/components/Logo"
 
 export function Sidebar() {
     const path = usePathname()
-    const [isMenuOpen, setIsMenuOpen] = useState('resources')
-    console.log(isMenuOpen)
     return (
-        <aside className="z-50 w-67 hidden lg:block max-2xl:w-15 bg-(--color-base-100) h-svh overflow-y-auto overflow-x-hidden border-r-1 border-(--color-gray-700) scrollbar-thin transition-all duration-200 group hover:max-2xl:w-67">
+        <aside className="z-50 w-67 hidden lg:block max-2xl:w-15 bg-(--color-base-100) h-svh overflow-y-auto overflow-x-hidden border-r-1 border-(--color-gray-700) scrollbar-thin transition-all duration-100 group hover:max-2xl:w-67">
+            <div className="grid grid-cols-[2.5em_1fr] items-center gap-2 px-3 py-2 h-16 border-b-1 border-(--color-gray-700)">
+                <Logo className="w-10 h-10 text-(--color-primary)" />
+                <h1 className="items-center h-full pt-1.5 text-xl font-bold text-(--color-warning) max-2xl:hidden group-hover:max-2xl:flex" style={{ fontFamily: "var(--font-josefin-sans)" }}>DevBraineer</h1>
+            </div>
             {<nav className="sticky top-4">
-
-                <div className="flex items-center gap-2 mb-8">
-                    <div className="w-10 h-10 rounded-md flex items-center justify-center animate-pulse-glow">
-                        <Code2 className="w-6 h-6 text-white" />
-                    </div>
-                    <span className="font-bold text-xl bg-clip-text text-transparent">DevBraineer</span>
-                </div>
-
                 {menuData.map((item) => (
                     <MenuItem key={item.label} item={item} currentPath={path} />
                 ))}
-
             </nav >}
         </aside >
     )
