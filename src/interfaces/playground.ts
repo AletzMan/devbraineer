69df42caeb7bba00f30513cd3d85deca52dda441
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-05-18 23:12:56 -0600

    feat: Define interfaces for code output and response in Playground

---
+++ src/interfaces/playground.ts
@@ -0,0 +1,18 @@
+export interface IOutputRunCode {
+    language: string,
+    version: string,
+    run: IOutputRun
+}
+
+export interface IResponseOutputRunCode {
+    error: boolean,
+    response: IOutputRunCode | null,
+}
+
+export interface IOutputRun {
+    stdout: string,
+    stderr: string,
+    code: number,
+    signal: unknown,
+    output: string
+}
