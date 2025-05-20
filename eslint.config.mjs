e5753c83cb494f56ba248e2ce4d49d8025f71ed6
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-05-20 05:50:12 -0600

    chore: Update eslint configuration for consistent code style

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,32 +1,36 @@
-import { dirname } from "path"; 
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc"; 
-import prettierConfig from "./prettier.config.js";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import prettierConfig from './prettier.config.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.config({
-    extends: ["next/core-web-vitals", "next/typescript", 'plugin:prettier/recommended' ], 
-    plugins: ["prettier"],
-    rules: { 
-      "prettier/prettier": ["error", prettierConfig],
-      semi: ["error", "always"],
-      "@typescript-eslint/no-unused-vars": [
-        "warn", // o "error"
-        {
-          argsIgnorePattern: "^_",
-          varsIgnorePattern: "^_",
-          caughtErrorsIgnorePattern: "^_",
+    ...compat.config({
+        extends: [
+            'next/core-web-vitals',
+            'next/typescript',
+            'plugin:prettier/recommended',
+        ],
+        plugins: ['prettier'],
+        rules: {
+            'prettier/prettier': ['error', prettierConfig],
+            semi: ['error', 'always'],
+            '@typescript-eslint/no-unused-vars': [
+                'warn', // o "error"
+                {
+                    argsIgnorePattern: '^_',
+                    varsIgnorePattern: '^_',
+                    caughtErrorsIgnorePattern: '^_',
+                },
+            ],
         },
-      ],
-    },
-  }),
+    }),
 ];
 
 export default eslintConfig;
