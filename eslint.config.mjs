8d28dbf40d9b667197ea34058ff463735ab1b7e4
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-05-19 21:44:45 -0600

    * chore(eslint.config.mjs): update eslint configuration to include prettier integration and enforce consistent semicolons

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,22 @@
-import { dirname } from "path"
-import { fileURLToPath } from "url"
-import { FlatCompat } from "@eslint/eslintrc"
+import { dirname } from "path"; 
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc"; 
+import prettierConfig from "./prettier.config.js";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-})
+});
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
   ...compat.config({
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+    extends: ["next/core-web-vitals", "next/typescript", 'plugin:prettier/recommended' ], 
+    plugins: ["prettier"],
+    rules: { 
+      "prettier/prettier": ["error", prettierConfig],
+      semi: ["error", "always"],
       "@typescript-eslint/no-unused-vars": [
         "warn", // o "error"
         {
@@ -26,10 +25,8 @@ const eslintConfig = [
           caughtErrorsIgnorePattern: "^_",
         },
       ],
-      'semi': ['error', 'always'],
     },
-  })
+  }),
+];
 
-]
-
-export default eslintConfig
+export default eslintConfig;
