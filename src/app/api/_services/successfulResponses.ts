736f04786c3ea14db0fa974cee7ccee8a5de1288
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-05-19 21:47:42 -0600

    style: Update success response functions for consistent code formatting and readability

--- src/app/api/_services/successfulResponses.ts
+++ src/app/api/_services/successfulResponses.ts
@@ -1,64 +1,64 @@
-import { NextResponse } from "next/server"
+import { NextResponse } from 'next/server';
 
 export const PaginationResponse = (
-	results: unknown,
-	totalResults: number,
-	totalPages: number,
-	currentPage: number
+    results: unknown,
+    totalResults: number,
+    totalPages: number,
+    currentPage: number
 ) => {
-	return NextResponse.json(
-		{
-			error: false,
-			response: {
-				results,
-				totalResults,
-				totalPages,
-				currentPage,
-			},
-		},
-		{ status: 200 }
-	)
-}
+    return NextResponse.json(
+        {
+            error: false,
+            response: {
+                results,
+                totalResults,
+                totalPages,
+                currentPage,
+            },
+        },
+        { status: 200 }
+    );
+};
 
 export const SuccessUpdate = (data: unknown) => {
-	return NextResponse.json(
-		{
-			error: false,
-			message: "Updated successfully",
-			data: data,
-		},
-		{ status: 200 }
-	)
-}
+    return NextResponse.json(
+        {
+            error: false,
+            message: 'Updated successfully',
+            data: data,
+        },
+        { status: 200 }
+    );
+};
 
 export const SuccessDelete = (data: unknown) => {
-	return NextResponse.json(
-		{
-			error: false,
-			message: "Deleted successfully",
-			data: data
-		},
-		{ status: 200 }
-	)
-}
+    return NextResponse.json(
+        {
+            error: false,
+            message: 'Deleted successfully',
+            data: data,
+        },
+        { status: 200 }
+    );
+};
 
 export const SuccessCreate = (data: unknown) => {
-	return NextResponse.json(
-		{
-			error: false,
-			message: "Created successfully",
-			data: data,
-		},
-		{ status: 201 }
-	)
-}
+    return NextResponse.json(
+        {
+            error: false,
+            message: 'Created successfully',
+            data: data,
+        },
+        { status: 201 }
+    );
+};
 
 export const SuccessResponse = (data: unknown) => {
-	return NextResponse.json(
-		{
-			error: false,
-			response: data,
-		},
-		{ status: 200 }
-	)
-}
+    return NextResponse.json(
+        {
+            error: false,
+            response: data,
+        },
+        { status: 200 }
+    );
+};
