e36cb05226b21d8129053558eb66ff8b6d287af3
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-05-20 05:56:44 -0600

    ``` chore: Update eslint and image configurations for consistent code style and optimization ```

--- src/app/platform/componentes/Header.tsx
+++ src/app/platform/componentes/Header.tsx
@@ -1,27 +1,40 @@
-"use client"
-import { UserButton } from "@clerk/nextjs"
-import { dark } from "@clerk/themes"
-import { useState } from "react"
-import { usePathname } from "next/navigation"
-import { menuData } from "../constants"
+'use client';
+import { UserButton } from '@clerk/nextjs';
+import { dark } from '@clerk/themes';
+import { usePathname } from 'next/navigation';
 
 export default function Header() {
-    const [theme, setTheme] = useState("light")
-    const path = usePathname()
-    console.log(path)
+    const path = usePathname();
     return (
         <header className=" flex items-center justify-between h-16 w-full bg-(--color-base-100) border-b-1 border-(--color-gray-700) z-50 px-2">
             <div className="breadcrumbs text-sm">
                 <ul>
-                    {path.split("/").map((item, index) => index > 0 && (
-                        <li key={index} className="capitalize text-yellow-400">
-                            {index === path.split("/").length - 1 && item}
-                            {index > 0 && index < path.split("/").length - 1 && <a className="text-gray-400" href={`/${item}`}>{item}</a>}
-                        </li>
-                    ))}
+                    {path.split('/').map(
+                        (item, index) =>
+                            index > 0 && (
+                                <li
+                                    key={index}
+                                    className="capitalize text-yellow-400">
+                                    {index === path.split('/').length - 1 &&
+                                        item}
+                                    {index > 0 &&
+                                        index < path.split('/').length - 1 && (
+                                            <a
+                                                className="text-gray-400"
+                                                href={`/${item}`}>
+                                                {item}
+                                            </a>
+                                        )}
+                                </li>
+                            )
+                    )}
                 </ul>
             </div>
-            <UserButton appearance={{ baseTheme: dark }} userProfileMode="navigation" userProfileUrl="/profile" />
+            <UserButton
+                appearance={{ baseTheme: dark }}
+                userProfileMode="navigation"
+                userProfileUrl="/profile"
+            />
         </header>
-    )
+    );
 }
