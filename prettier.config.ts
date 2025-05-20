ab6a71316bf10500eaeb69b113bb31853be19f1a
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-05-20 05:43:06 -0600

    chore: Add prettier configuration file for consistent code formatting

---
+++ prettier.config.ts
@@ -0,0 +1,9 @@
+module.exports = {
+    semi: true,
+    singleQuote: true,
+    bracketSameLine: true,
+    trailingComma: 'es5',
+    tabWidth: 4,
+    jsxBracketSameLine: true,
+    endOfLine: 'auto',
+};
