5e3a178272a9607a97d2aa457a9b14c2bc607995
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-05-20 21:57:47 -0600

    feat: Implement JSON Schema to Zod conversion in generateSchemas function

---
+++ src/services/converter.service.ts
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+interface SchemaResponse {
+    message: string;
+    data: Schemas;
+    error?: string;
+}
+
+interface Schemas {
+    jsonSchema: string;
+    zodSchema: string;
+}
+
+export async function generateSchemas(
+    jsonData: any
+): Promise<Schemas | undefined> {
+    try {
+        const res = await axios.post<SchemaResponse>(
+            '/api/converter/generate-schema',
+            jsonData
+        );
+        const response: Schemas = res.data.data;
+        if (res.status === 201) {
+            return response;
+        }
+        return undefined;
+    } catch (error) {
+        console.error('Error al generar el esquema:', error);
+        return undefined;
+    }
+}
