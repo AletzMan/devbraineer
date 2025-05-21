801a4eb9d52cb44462b5280a6170ae6a74ffe6e0
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-05-21 16:21:37 -0600

    ``` refactor: Improve breadcrumb generation in Header component using LinkData ```

--- src/app/platform/componentes/Header.tsx
+++ src/app/platform/componentes/Header.tsx
@@ -6,18 +6,15 @@ import { LinkData } from '../constants';
 
 export default function Header() {
     const path = usePathname();
-    const pathSegments = path.split('/').filter((segment) => segment !== ''); // Filtra los segmentos vacíos ('')
+    const pathSegments = path.split('/').filter((segment) => segment !== '');
 
     return (
         <header className="flex items-center justify-between h-16 w-full bg-base-100 border-b border-gray-700 z-50 px-2">
             <div className="text-sm breadcrumbs">
                 <ul>
                     {pathSegments.map((item, index) => {
-                        // Reconstruye el href acumulativo para cada segmento.
-                        // Esto DEBE coincidir exactamente con los hrefs en LinkData
                         const currentHref = `/${pathSegments.slice(0, index + 1).join('/')}`;
 
-                        // Encuentra el objeto IMenuItem que coincide exactamente con el currentHref
                         const currentLinkItem = LinkData.find(
                             (menuItem) => menuItem.href === currentHref
                         );
@@ -25,11 +22,9 @@ export default function Header() {
                         const isFirstSegment = index === 0;
                         const isLastSegment = index === pathSegments.length - 1;
 
-                        // Mostrar icono para todos los segmentos excepto el primero ('platform')
                         const shouldShowIcon =
                             !isFirstSegment && currentLinkItem?.icon;
 
-                        // Clase para el color del texto
                         const textColorClass = isLastSegment
                             ? `${currentLinkItem?.color}`
                             : 'text-gray-400';
