bcacb72db4eb1266f3e22290466e52291ccde8b7
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-05-21 18:22:06 -0600

    refactor: Improve history management in storage module

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -7,6 +7,22 @@ export const saveHistoryToLocalStorage = (history: RequestHistory) => {
 };
 
 export const getHistoryFromLocalStorage = () => {
-    const history = localStorage.getItem('http-history');
-    return history ? JSON.parse(history) : [];
+    const history: RequestHistory[] = localStorage.getItem('http-history')
+        ? JSON.parse(localStorage.getItem('http-history') || '[]')
+        : [];
+    return history;
+};
+
+export const deleteHistoryFromLocalStorage = () => {
+    localStorage.removeItem('http-history');
+};
+
+export const deleteHistoryItemFromLocalStorage = (id: string) => {
+    console.log(id);
+    const existingHistory: RequestHistory[] = getHistoryFromLocalStorage();
+    console.log(existingHistory);
+    const updatedHistory = existingHistory.filter((h: RequestHistory) => h.id !== id);
+    console.log(updatedHistory);
+    localStorage.setItem('http-history', JSON.stringify(updatedHistory));
+    console.log(localStorage.getItem('http-history'));
 };
