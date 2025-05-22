d85b964f39f74773af6495cde5fb30077eeb875b
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-05-21 23:24:07 -0600

    feat: Implement GET and DELETE methods for notes API endpoints

---
+++ src/app/api/notes/[noteId]/route.ts
@@ -0,0 +1,68 @@
+import { auth } from '@clerk/nextjs/server';
+import { NotAuthorizedError, NotFoundError, UnprocessableEntityError } from '../../_services/errors';
+import prisma from '@/lib/db';
+import { SuccessResponse } from '../../_services/successfulResponses';
+import { ServerError } from '../../_services/errors';
+
+export async function GET(req: Request) {
+    const { userId } = await auth();
+
+    if (!userId) {
+        return NotAuthorizedError();
+    }
+
+    try {
+        const { searchParams } = new URL(req.url);
+        const id = searchParams.get('id');
+
+        if (!id) {
+            return UnprocessableEntityError([{ message: 'ID is required' }]);
+        }
+
+        const note = await prisma.note.findUnique({
+            where: { id },
+        });
+
+        if (!note || note.userId !== userId) {
+            return NotAuthorizedError();
+        }
+
+        return SuccessResponse(note);
+    } catch (error) {
+        console.error('Error fetching note:', error);
+        return ServerError();
+    }
+}
+
+// DELETE /api/notes/:id
+export async function DELETE(req: Request, { params }: { params: Promise<{ noteId: string }> }) {
+    const { userId } = await auth();
+
+    if (!userId) {
+        return NotAuthorizedError();
+    }
+
+    try {
+        const noteId = (await params).noteId;
+        if (!noteId) {
+            return UnprocessableEntityError([{ message: 'ID is required' }]);
+        }
+
+        const note = await prisma.note.findUnique({
+            where: { id: noteId },
+        });
+
+        if (!note) {
+            return NotFoundError();
+        }
+
+        await prisma.note.delete({
+            where: { id: noteId },
+        });
+
+        return SuccessResponse({ message: 'Note deleted successfully' });
+    } catch (error) {
+        console.error('Error deleting note:', error);
+        return ServerError();
+    }
+}
