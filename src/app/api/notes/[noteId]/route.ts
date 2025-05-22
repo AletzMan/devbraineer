195924d6c854e48ea1561cacc9d121695cec2238
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-05-22 01:05:07 -0600

    feat: Implement PUT method for updating notes with validation and error handling

--- src/app/api/notes/[noteId]/route.ts
+++ src/app/api/notes/[noteId]/route.ts
@@ -1,8 +1,10 @@
 import { auth } from '@clerk/nextjs/server';
 import { NotAuthorizedError, NotFoundError, UnprocessableEntityError } from '../../_services/errors';
 import prisma from '@/lib/db';
-import { SuccessResponse } from '../../_services/successfulResponses';
+import { SuccessResponse, SuccessUpdate } from '../../_services/successfulResponses';
 import { ServerError } from '../../_services/errors';
+import { updateNoteSchema } from '@/lib/schemas/note';
+import { ZodError } from 'zod';
 
 export async function GET(req: Request) {
     const { userId } = await auth();
@@ -63,6 +65,46 @@ export async function DELETE(req: Request, { params }: { params: Promise<{ noteI
         return SuccessResponse({ message: 'Note deleted successfully' });
     } catch (error) {
         console.error('Error deleting note:', error);
+        return ServerError();
+    }
+}
+
+export async function PUT(req: Request, { params }: { params: Promise<{ noteId: string }> }) {
+    const { userId } = await auth();
+
+    if (!userId) {
+        return NotAuthorizedError();
+    }
+
+    try {
+        const noteId = (await params).noteId;
+        if (!noteId) {
+            return UnprocessableEntityError([{ message: 'ID is required' }]);
+        }
+
+        const validatedNote = updateNoteSchema.parse(await req.json());
+
+        const note = await prisma.note.findUnique({
+            where: { id: noteId },
+        });
+
+        if (!note) {
+            return NotFoundError();
+        }
+
+        const updatedNote = await prisma.note.update({
+            where: { id: noteId },
+            data: validatedNote,
+        });
+        if (updatedNote) {
+            return SuccessUpdate(updatedNote);
+        }
+        return NotFoundError();
+    } catch (error) {
+        if (error instanceof ZodError) {
+            return UnprocessableEntityError(error.issues);
+        }
+        console.error('Error updating note:', error);
         return ServerError();
     }
 }
