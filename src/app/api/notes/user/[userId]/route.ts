5cd278758ea3048388160cfdbf49fcb2afc1bd1c
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-05-21 23:24:10 -0600

    feat: Implement GET method for fetching notes by user ID

---
+++ src/app/api/notes/user/[userId]/route.ts
@@ -0,0 +1,31 @@
+import { auth } from '@clerk/nextjs/server';
+import prisma from '@/lib/db';
+import { NotAuthorizedError, NotFoundError, ServerError } from '@/app/api/_services/errors';
+import { SuccessResponse } from '@/app/api/_services/successfulResponses';
+
+export async function GET(req: Request, { params }: { params: Promise<{ userId: string }> }) {
+    try {
+        const { userId } = await auth();
+
+        const idUser = (await params).userId;
+        if (!userId) {
+            return NotAuthorizedError();
+        }
+        console.log(idUser);
+        console.log(userId);
+        const notes = await prisma.note.findMany({
+            where: {
+                userId: idUser,
+            },
+        });
+        console.log(notes);
+        if (notes) {
+            return SuccessResponse(notes);
+        }
+
+        return NotFoundError();
+    } catch (error) {
+        console.error('Error fetching note:', error);
+        return ServerError();
+    }
+}
