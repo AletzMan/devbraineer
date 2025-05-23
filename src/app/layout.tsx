195791876b6dd653ed424e97f86eb495268871d0
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-05-22 23:29:40 -0600

    refactor: Update RootLayout to use ThemeProvider for managing font variables

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,21 +2,10 @@ import type { Metadata } from 'next';
 import { Jost, Josefin_Sans, Inconsolata } from 'next/font/google';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
-
-const jost = Jost({
-    variable: '--font-jost',
-    subsets: ['latin'],
-});
-
-const josefinSans = Josefin_Sans({
-    variable: '--font-josefin-sans',
-    subsets: ['latin'],
-});
-
-const code = Inconsolata({
-    variable: '--font-code',
-    subsets: ['latin'],
-});
+import { ThemeProvider } from '@/app/components/ThemeProvider';
+const jost = Jost({ variable: '--font-jost', subsets: ['latin'] });
+const josefinSans = Josefin_Sans({ variable: '--font-josefin-sans', subsets: ['latin'] });
+const code = Inconsolata({ variable: '--font-code', subsets: ['latin'] });
 
 export const metadata: Metadata = {
     title: 'DevBraineer',
@@ -26,17 +15,15 @@ export const metadata: Metadata = {
     },
 };
 
-export default function RootLayout({
-    children,
-}: Readonly<{
-    children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
-        <html lang="en" data-theme="dark">
+        <html lang="en">
             <ClerkProvider>
-                <body className={`${jost.variable} ${josefinSans.variable} ${code.variable} antialiased`}>
-                    {children}
-                </body>
+                <ThemeProvider>
+                    <body className={`${jost.variable} ${josefinSans.variable} ${code.variable} antialiased`}>
+                        {children}
+                    </body>
+                </ThemeProvider>
             </ClerkProvider>
         </html>
     );
