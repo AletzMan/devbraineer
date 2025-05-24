5049cff2d853f5c2404c42f4f617d6fff8e97a98
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-05-23 22:39:41 -0600

    refactor: Update ThemeController to use next-themes for theme management and adjust theme toggling logic

--- src/app/components/ThemeController.tsx
+++ src/app/components/ThemeController.tsx
@@ -1,17 +1,16 @@
 'use client';
-import { useTheme } from '@/app/components/ThemeProvider';
 import { MoonIcon, SunIcon } from 'lucide-react';
+import { useTheme } from 'next-themes';
 
 export const ThemeController = () => {
-    const { toggleTheme, theme } = useTheme();
-    const label = theme === 'dark' ? 'Switch to light theme' : 'Switch to dark theme';
+    const { theme, setTheme } = useTheme();
     return (
         <label className="swap swap-rotate">
             <input
                 type="checkbox"
                 checked={theme === 'dark'}
-                onChange={() => toggleTheme()}
-                className="theme-controller hidden"
+                onChange={() => setTheme(theme === 'dark' ? 'cupcake' : 'dark')}
+                className="theme-controller"
             />
             <SunIcon className="swap-off size-6 text-gray-600" />
             <MoonIcon className="swap-on size-6 text-gray-400" />
