8c5083959a52df0ce9017d1fcb43b582fc4fd28a
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-05-24 11:47:16 -0600

    style: Update ResponseViewer component styles for improved consistency and readability

--- src/app/platform/playground/http-client/components/ResponseViewer.tsx
+++ src/app/platform/playground/http-client/components/ResponseViewer.tsx
@@ -1,5 +1,7 @@
 import JsonView from '@uiw/react-json-view';
 import { monokaiTheme } from '@uiw/react-json-view/monokai';
+import { themes } from 'prism-react-renderer';
+import { CodeBlock } from 'react-code-block';
 
 interface ResponseViewerProps {
     response: {
@@ -35,13 +37,17 @@ export function ResponseViewer({ response }: ResponseViewerProps) {
         return {};
     })();
 
-    function looksLikeHTML(str: string) {
-        return /<\/?[a-z][\s\S]*>/i.test(str);
-    }
+    const looksLikeHTML = (str: string) => /<\/?[a-z][\s\S]*>/i.test(str);
+    const looksLikeXML = (str: string) => /<\/?[a-z][\s\S]*>/i.test(str);
+    const isXML = looksLikeXML(body);
+    console.log(body);
+    console.log(isXML);
+
+    const isArray = Array.isArray(body);
 
     return (
-        <div className="space-y-4">
-            <div className="flex flex-wrap items-center gap-3 text-sm mt-2">
+        <div className="space-y-4 h-full">
+            <div className="flex flex-wrap items-center gap-3 text-sm bg-base-200 p-2 rounded-sm">
                 {/* STATUS */}
                 <div className="flex items-center gap-1">
                     <span className="font-semibold text-base-content">Status:</span>{' '}
@@ -62,22 +68,45 @@ export function ResponseViewer({ response }: ResponseViewerProps) {
                     <span className="badge badge-outline text-accent">{size} KB</span>
                 </div>
             </div>
-
-            <div>
-                <h3 className="font-semibold mb-1">Headers</h3>
-                <div className="bg-base-200 p-3 rounded">
-                    <JsonView value={safeHeaders} style={monokaiTheme} />
-                </div>
-            </div>
-
-            <div>
-                <h3 className="font-semibold mb-1">Body</h3>
-                <div className="bg-base-200 p-3 rounded">
-                    {isObject(body) && !looksLikeHTML(body) ? (
-                        <JsonView value={body} style={monokaiTheme} />
-                    ) : (
-                        <pre className="whitespace-pre-wrap">{String(body)}</pre>
-                    )}
+            <div className=" bg-base-200 rounded-sm px-2 pb-2 h-full ">
+                <div className="tabs tabs-border h-full">
+                    <input type="radio" name="my_tabs_2" className="tab" aria-label="Body" defaultChecked />
+                    <div className="tab-content border-base-300 bg-base-100 p-5 h-full">
+                        <div className="h-full">
+                            <h3 className="font-semibold mb-1">Body</h3>
+                            <div className="bg-base-200 p-3 rounded h-full">
+                                {isObject(body) && !looksLikeHTML(body) && !looksLikeXML(body) ? (
+                                    <div className="h-full overflow-y-auto scrollbar-thin">
+                                        <JsonView value={body} style={monokaiTheme} />
+                                    </div>
+                                ) : (
+                                    <CodeBlock
+                                        code={String(body)}
+                                        language={isArray ? 'json' : looksLikeXML(body) ? 'xml' : 'html'}
+                                        theme={themes.oneDark}>
+                                        <div className="relative overflow-hidden h-full">
+                                            <CodeBlock.Code className="bg-neutral-900 overflow-hidden h-full p-4 rounded-b-sm shadow-lg border-1 border-gray-800 overflow-y-auto scrollbar-thin">
+                                                <div className="table-row">
+                                                    <CodeBlock.LineContent className="table-cell">
+                                                        <CodeBlock.Token />
+                                                    </CodeBlock.LineContent>
+                                                </div>
+                                            </CodeBlock.Code>
+                                        </div>
+                                    </CodeBlock>
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                    <input type="radio" name="my_tabs_2" className="tab" aria-label="Headers" />
+                    <div className="tab-content border-base-300 bg-base-100 p-5">
+                        <div>
+                            <h3 className="font-semibold mb-1">Headers</h3>
+                            <div className="bg-base-200 p-3 rounded">
+                                <JsonView value={safeHeaders} style={monokaiTheme} />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
