79f500e8f5bfbbc565e27689e14538bf1c2396ca
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-05-23 22:39:44 -0600

    feat: Add LayoutSubSection component for improved layout structure

---
+++ src/app/platform/componentes/LayoutSubSection.tsx
@@ -0,0 +1,13 @@
+interface LayoutSubSectionProps {
+    children: React.ReactNode;
+}
+
+export const LayoutSubSection = ({ children }: LayoutSubSectionProps) => {
+    return (
+        <div className="flex flex-col gap-2 p-2 h-svh ">
+            <div className="flex flex-col p-2 gap-2 border border-base-200 bg-base-100 rounded-sm h-[calc(100svh-5rem)] overflow-y-auto">
+                {children}
+            </div>
+        </div>
+    );
+};
