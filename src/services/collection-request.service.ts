af9966f9805fbc4f60a73348bbca4dd6cd5be83c
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-05-24 22:31:41 -0600

    feat: Add services for managing collections including adding, getting, deleting by name, deleting all, and deleting by ID

---
+++ src/services/collection-request.service.ts
@@ -0,0 +1,93 @@
+import { Collection, RequestHistory } from '@prisma/client';
+import axios from 'axios';
+
+interface RequestCollectionResponse {
+    error: boolean;
+    message?: string;
+    data?: CollectionsResponse;
+}
+
+export interface CollectionsResponse {
+    name: string;
+    id: string;
+    userId: string;
+    created_at: Date;
+    requests: RequestHistory[];
+}
+
+interface RequestCollectionsResponse {
+    error: boolean;
+    message?: string;
+    response?: CollectionsResponse[];
+}
+
+export const addToCollectionService = async (
+    request: RequestHistory,
+    name: string
+): Promise<CollectionsResponse | undefined> => {
+    try {
+        const response = await axios.post<RequestCollectionResponse>('/api/collections-request', { request, name });
+        if (response.status === 201) {
+            return response.data.data;
+        }
+        return undefined;
+    } catch (error) {
+        console.error('Error al agregar la petición a la colección:', error);
+        return undefined;
+    }
+};
+
+export const getCollectionsService = async (): Promise<CollectionsResponse[] | undefined> => {
+    try {
+        const response = await axios.get<RequestCollectionsResponse>('/api/collections-request');
+        if (response.status === 200) {
+            return response.data.response;
+        }
+        return undefined;
+    } catch (error) {
+        console.error('Error al obtener las colecciones:', error);
+        return undefined;
+    }
+};
+
+export const deleteCollectionService = async (name: string): Promise<CollectionsResponse | undefined> => {
+    try {
+        const response = await axios.delete<RequestCollectionResponse>(`/api/collections-request/byName`, {
+            data: { name },
+        });
+
+        if (response.status === 200) {
+            return response.data.data;
+        }
+        return undefined;
+    } catch (error) {
+        console.error('Error al eliminar la colección:', error);
+        return undefined;
+    }
+};
+
+export const deleteCollectionsService = async (): Promise<CollectionsResponse[] | undefined> => {
+    try {
+        const response = await axios.delete<RequestCollectionsResponse>('/api/collections-request');
+        if (response.status === 200) {
+            return response.data.response;
+        }
+        return undefined;
+    } catch (error) {
+        console.error('Error al eliminar las colecciones:', error);
+        return undefined;
+    }
+};
+
+export const deleteCollectionByIdService = async (id: string): Promise<CollectionsResponse | undefined> => {
+    try {
+        const response = await axios.delete<RequestCollectionResponse>(`/api/collections-request/requests/${id}`);
+        if (response.status === 200) {
+            return response.data.data;
+        }
+        return undefined;
+    } catch (error) {
+        console.error('Error al eliminar la colección:', error);
+        return undefined;
+    }
+};
