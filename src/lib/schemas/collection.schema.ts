d0a2fb4de81cbb9a875e57b86b3c91c555c5ee90
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-05-25 20:14:12 -0600

    refactor: Update createCollectionSchema to include optional request object for better structure

--- src/lib/schemas/collection.schema.ts
+++ src/lib/schemas/collection.schema.ts
@@ -1,12 +1,25 @@
 import { z } from 'zod';
 
-export const createCollectionSchema = z.object({
+/*export const createCollectionSchema = z.object({
     name: z.string().min(1),
     method: z.string(),
     url: z.string(),
     headers: z.array(z.object({ key: z.string(), value: z.string() })),
     body: z.string().optional(),
     response: z.string(),
+});*/
+
+export const createCollectionSchema = z.object({
+    name: z.string().min(1),
+    request: z
+        .object({
+            method: z.string().min(1),
+            url: z.string().url(),
+            headers: z.array(z.object({ key: z.string(), value: z.string() })).optional(),
+            body: z.string().optional(),
+            response: z.string().optional(),
+        })
+        .optional(), // <-- Hacemos opcional el request
 });
 
 export const updateCollectionSchema = createCollectionSchema.partial();
