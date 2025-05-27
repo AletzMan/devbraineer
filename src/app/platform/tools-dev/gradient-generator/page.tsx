a4f3d97c4f6fa4b0b7b80ba35673f309c1874722
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-05-27 00:52:26 -0600

    feat: Implement tabbed view for CSS and Tailwind gradient generators

--- src/app/platform/tools-dev/gradient-generator/page.tsx
+++ src/app/platform/tools-dev/gradient-generator/page.tsx
@@ -1,23 +1,52 @@
+'use client';
 import GradientGenerator from './components/GradientGenerator';
 import TailwindGradientGenerator from './components/TailwindGradientGenerator';
 import { LayoutSubSection } from '../../componentes/LayoutSubSection';
+import { useState, useEffect, useLayoutEffect } from 'react';
+import { useSearchParams } from 'next/navigation';
 
 export default function GradientGeneratorPage() {
+    const [tabGradient, setTabGradient] = useState<'css' | 'tailwind'>('css');
+    const params = useSearchParams();
+
+    useLayoutEffect(() => {
+        const type = params.get('type');
+        if (type === 'tailwind') {
+            setTabGradient('tailwind');
+        } else {
+            setTabGradient('css');
+        }
+    }, [params]);
+
+    const handleChangeTab = (tab: 'css' | 'tailwind') => {
+        const url = new URL(window.location.href);
+        url.searchParams.set('type', tab);
+        window.history.replaceState(null, '', url);
+    };
+
     return (
         <LayoutSubSection>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                <div className="card">
-                    <div className="card-body">
-                        <h2 className="card-title">Generador CSS</h2>
-                        <GradientGenerator />
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="card-body">
-                        <h2 className="card-title">Generador Tailwind</h2>
-                        <TailwindGradientGenerator />
-                    </div>
-                </div>
+            <div className="tabs tabs-box">
+                <input
+                    type="radio"
+                    name="tabGradient"
+                    className="tab"
+                    aria-label="Gradient CSS"
+                    checked={tabGradient === 'css'}
+                    onChange={() => handleChangeTab('css')}
+                />
+                <input
+                    type="radio"
+                    name="tabGradient"
+                    className="tab"
+                    aria-label="Gradient Tailwind"
+                    checked={tabGradient === 'tailwind'}
+                    onChange={() => handleChangeTab('tailwind')}
+                />
+            </div>
+            <div className="flex flex-col gap-2">
+                {tabGradient === 'css' && <GradientGenerator />}
+                {tabGradient === 'tailwind' && <TailwindGradientGenerator />}
             </div>
         </LayoutSubSection>
     );
