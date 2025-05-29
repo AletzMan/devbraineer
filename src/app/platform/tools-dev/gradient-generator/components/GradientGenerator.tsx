7e9861b5eabef050c9e1abdbd89f75b824356da0
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-05-29 00:41:21 -0600

    feat: Implement lazy loading for images in the gallery

--- src/app/platform/tools-dev/gradient-generator/components/GradientGenerator.tsx
+++ src/app/platform/tools-dev/gradient-generator/components/GradientGenerator.tsx
@@ -19,6 +19,10 @@ import {
     ArrowUpDown,
     CircleSmall,
     ClipboardPasteIcon,
+    RectangleHorizontal,
+    Circle,
+    Square,
+    Expand,
 } from 'lucide-react';
 import { DndProvider, useDrag, useDrop } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
@@ -41,6 +45,8 @@ export default function GradientGenerator() {
     const [rotation, setRotation] = useState(0);
     const [centerX, setCenterX] = useState(50);
     const [centerY, setCenterY] = useState(50);
+    const [sizeView, setSizeView] = useState(1);
+    const [typeView, setTypeView] = useState<'rectangle' | 'circle' | 'square'>('rectangle');
     const debouncedUpdateStops = useMemo(
         () =>
             useDebounce((stops: GradientStop[]) => {
@@ -218,7 +224,7 @@ export default function GradientGenerator() {
                         />
                     </label>
                     <div className="flex items-center justify-center text-xs w-12 text-center bg-base-content/10 rounded-sm h-6">
-                        {stop.position * 100}%
+                        {(stop.position * 100).toFixed(0)}%
                     </div>
                     <button onClick={onRemove} className="btn btn-soft btn-error btn-xs">
                         <Trash2 className="size-4" />
@@ -278,8 +284,8 @@ export default function GradientGenerator() {
                 <div className="flex flex-col gap-2 bg-base-100 shadow-xl h-[calc(100svh-10rem)] overflow-hidden">
                     <div className="p-0">
                         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                            <div className="flex flex-col gap-4 bg-base-200 p-4 rounded-sm">
-                                <div className="grid grid-cols-2 gap-4 mb-8 w-full">
+                            <div className="flex flex-col gap-2 bg-base-100 border border-base-300 p-2 rounded-sm">
+                                <div className="grid grid-cols-2 gap-4 w-full bg-base-200 p-2 rounded-sm">
                                     <label className="label flex flex-col items-start ">
                                         <span className="label-text text-sm">Tipo de gradiente</span>
                                         <select
@@ -314,7 +320,7 @@ export default function GradientGenerator() {
                                     {gradientType === 'radial' && (
                                         <label className="label flex flex-col items-start ">
                                             <span className="label-text text-sm">Centro del gradiente</span>
-                                            <div className="flex flex-col bg-base-content/5 rounded-sm p-0.5">
+                                            <div className="flex flex-col">
                                                 <div className="flex gap-2">
                                                     <input
                                                         type="range"
@@ -353,47 +359,50 @@ export default function GradientGenerator() {
                                         </label>
                                     )}
                                 </div>
-                                <div className="">
-                                    <Slider
-                                        className="handler_positions"
-                                        min={0}
-                                        styles={{
-                                            handle: {
-                                                backgroundColor: 'var(--color-base-200)',
-                                                borderColor: 'var(--color-base-content)',
-                                                opacity: 1,
-                                                borderRadius: '2px',
-                                                borderWidth: '1px',
-                                                height: '24px',
-                                                width: '8px',
-                                                transform: 'translateY(-5px) translateX(-4px)',
-                                            },
-                                            track: {
-                                                backgroundColor: 'var(--color-primary)',
-                                                height: '4px',
-                                            },
-                                            rail: {
-                                                backgroundColor: 'var(--color-base-content)',
-                                                height: '4px',
-                                            },
-                                        }}
-                                        range={{ draggableTrack: true }}
-                                        allowCross={false}
-                                        max={1}
-                                        value={gradientStops.map((stop) => stop.position)}
-                                        onChange={(values) => {
-                                            if (!Array.isArray(values)) {
-                                                return;
-                                            }
-                                            setGradientStops(
-                                                gradientStops.map((stop, index) => ({
-                                                    ...stop,
-                                                    position: values[index],
-                                                }))
-                                            );
-                                        }}
-                                        step={0.01}
-                                    />
+                                <div className="bg-base-200 p-4 pt-2 pb-4 rounded-sm">
+                                    <label className="label flex flex-col items-center gap-7">
+                                        <span className="label-text text-sm">Posiciones</span>
+                                        <Slider
+                                            className="handler_positions"
+                                            min={0}
+                                            styles={{
+                                                handle: {
+                                                    backgroundColor: 'var(--color-base-200)',
+                                                    borderColor: 'var(--color-base-content)',
+                                                    opacity: 1,
+                                                    borderRadius: '2px',
+                                                    borderWidth: '1px',
+                                                    height: '24px',
+                                                    width: '8px',
+                                                    transform: 'translateY(-5px) translateX(-4px)',
+                                                },
+                                                track: {
+                                                    backgroundColor: 'var(--color-primary)',
+                                                    height: '4px',
+                                                },
+                                                rail: {
+                                                    backgroundColor: 'var(--color-base-content)',
+                                                    height: '4px',
+                                                },
+                                            }}
+                                            range={{ draggableTrack: true }}
+                                            allowCross={false}
+                                            max={1}
+                                            value={gradientStops.map((stop) => stop.position)}
+                                            onChange={(values) => {
+                                                if (!Array.isArray(values)) {
+                                                    return;
+                                                }
+                                                setGradientStops(
+                                                    gradientStops.map((stop, index) => ({
+                                                        ...stop,
+                                                        position: values[index],
+                                                    }))
+                                                );
+                                            }}
+                                            step={0.01}
+                                        />
+                                    </label>
                                 </div>
                                 <div className="grid grid-cols-2 gap-2">
                                     <button onClick={handleAddStop} className="btn btn-accent btn-sm w-full">
@@ -417,30 +426,82 @@ export default function GradientGenerator() {
                                     ))}
                                 </div>
                             </div>
-                            <div className="mb-8 bg-base-200 p-2 rounded-sm h-full">
-                                <div className="flex items-center gap-4 mb-6">
-                                    <Eye className="h-5 w-5 text-muted-foreground" />
-                                    <h3 className="font-semibold">Vista Previa</h3>
+                            <div className="flex flex-col gap-2 mb-8 bg-base-100 border border-base-300 p-2 rounded-sm h-full">
+                                <div className="flex gap-4 w-full">
+                                    <div className="flex items-center gap-2 w-full">
+                                        <Eye className="h-5 w-5 text-muted-foreground " />
+                                        <h3 className="font-semibold w-full">Vista Previa</h3>
+                                    </div>
+                                    <label className="label flex flex-col items-start w-full">
+                                        <span className="label-text text-sm">Tamaño</span>
+                                        <input
+                                            type="range"
+                                            min="0.1"
+                                            max="1"
+                                            step="0.1"
+                                            value={sizeView}
+                                            className="range range-primary w-full"
+                                            onChange={(e) => setSizeView(Number(e.target.value))}
+                                        />
+                                    </label>
+                                    <label className="label flex flex-col items-start">
+                                        <span className="label-text text-sm">Tipo de Vista</span>
+                                        <div className="join">
+                                            <button
+                                                className={`join-item btn btn-sm btn-soft ${typeView === 'rectangle' ? 'btn-active btn-primary' : 'btn-default'}`}
+                                                type="button"
+                                                name="options"
+                                                onClick={() => setTypeView('rectangle')}>
+                                                <RectangleHorizontal className="size-4" />
+                                            </button>
+                                            <button
+                                                className={`join-item btn btn-sm btn-soft ${typeView === 'circle' ? 'btn-active btn-primary' : 'btn-default'}`}
+                                                type="button"
+                                                name="options"
+                                                onClick={() => setTypeView('circle')}>
+                                                <Circle className="size-4" />
+                                            </button>
+                                            <button
+                                                className={`join-item btn btn-sm btn-soft ${typeView === 'square' ? 'btn-active btn-primary' : 'btn-default'}`}
+                                                type="button"
+                                                name="options"
+                                                onClick={() => setTypeView('square')}>
+                                                <Square className="size-4" />
+                                            </button>
+                                        </div>
+                                    </label>
                                 </div>
 
-                                <div className="mb-8 p-2 bg-lines border border-base-300 rounded-sm">
+                                <div className="relative mb-8 p-2 bg-lines border border-base-300 rounded-sm overflow-hidden">
                                     <div
-                                        className="relative preview-container h-70 w-full rounded-sm"
-                                        style={{ background: generateCSS() }}>
-                                        <div className="absolute inset-0 p-4 bg-black/50 opacity-0 hover:opacity-100 transition-opacity duration-300 flex flex-col items-center justify-center">
-                                            <div className="flex flex-col gap-2">
-                                                <pre className="text-center text-sm text-muted-foreground p-4 max-w-[65ch] text-pretty max-h-55 overflow-y-auto scrollbar-thin">
-                                                    {`background: ${generateCSS().replace(/((?:[^,]+, ){2})/g, '$1\n')}`}
-                                                </pre>
-                                                <button onClick={handleCopy} className="btn btn-secondary btn-sm">
-                                                    {copied ? 'Copiado!' : 'Copiar Clases'}
-                                                    {copied ? (
-                                                        <Check className="h-4 w-4 ml-2" />
-                                                    ) : (
-                                                        <Copy className="h-4 w-4 ml-2" />
-                                                    )}
-                                                </button>
-                                            </div>
+                                        className="preview-container w-full h-full rounded-sm"
+                                        style={{
+                                            background: generateCSS(),
+                                            transform: `scale(${sizeView})`,
+                                            borderRadius: typeView === 'circle' ? '50%' : '0.25em',
+                                            aspectRatio:
+                                                typeView === 'rectangle'
+                                                    ? '2/1'
+                                                    : typeView === 'square' || typeView === 'circle'
+                                                      ? '1/1'
+                                                      : 'auto',
+                                            height: typeView === 'rectangle' ? '50%' : '100%',
+                                            marginTop: typeView === 'rectangle' ? '17%' : '0',
+                                        }}
+                                    />
+                                    <div className="absolute inset-0 p-4 bg-black/50 opacity-0 hover:opacity-100 transition-opacity duration-300 flex flex-col items-center justify-center">
+                                        <div className="flex flex-col gap-2">
+                                            <pre className="text-center text-sm text-muted-foreground p-4 max-w-[65ch] text-pretty max-h-55 overflow-y-auto scrollbar-thin">
+                                                {`background: ${generateCSS().replace(/((?:[^,]+, ){2})/g, '$1\n')}`}
+                                            </pre>
+                                            <button onClick={handleCopy} className="btn btn-secondary btn-sm">
+                                                {copied ? 'Copiado!' : 'Copiar Clases'}
+                                                {copied ? (
+                                                    <Check className="h-4 w-4 ml-2" />
+                                                ) : (
+                                                    <Copy className="h-4 w-4 ml-2" />
+                                                )}
+                                            </button>
                                         </div>
                                     </div>
                                 </div>
