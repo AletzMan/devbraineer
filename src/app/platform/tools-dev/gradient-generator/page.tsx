4b6e1da10bdfba185578fa2b2f8635330f562eab
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-05-29 18:05:44 -0600

    refactor: Update GradientGeneratorPage to use GradientComponent for improved functionality

--- src/app/platform/tools-dev/gradient-generator/page.tsx
+++ src/app/platform/tools-dev/gradient-generator/page.tsx
@@ -1,56 +1,16 @@
 'use client';
-import GradientGenerator from './components/GradientGenerator';
-import TailwindGradientGenerator from './components/TailwindGradientGenerator';
-import { LayoutSubSection } from '../../componentes/LayoutSubSection';
-import { useState, useEffect, useLayoutEffect, Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
-import { Toaster } from 'react-hot-toast';
+import { LayoutSubSection } from '../../componentes/LayoutSubSection';
+import GradientComponent from './components/GradientComponent';
+import { Suspense } from 'react';
 
 export default function GradientGeneratorPage() {
-    const [tabGradient, setTabGradient] = useState<'css' | 'tailwind'>('css');
     const params = useSearchParams();
-
-    useLayoutEffect(() => {
-        const type = params.get('type');
-        if (type === 'tailwind') {
-            setTabGradient('tailwind');
-        } else {
-            setTabGradient('css');
-        }
-    }, [params]);
-
-    const handleChangeTab = (tab: 'css' | 'tailwind') => {
-        const url = new URL(window.location.href);
-        url.searchParams.set('type', tab);
-        window.history.replaceState(null, '', url);
-    };
 
     return (
         <LayoutSubSection>
-            <Toaster position="top-center" />
-            <div className="tabs tabs-box">
-                <input
-                    type="radio"
-                    name="tabGradient"
-                    className="tab"
-                    aria-label="Gradient CSS"
-                    checked={tabGradient === 'css'}
-                    onChange={() => handleChangeTab('css')}
-                />
-                <input
-                    type="radio"
-                    name="tabGradient"
-                    className="tab"
-                    aria-label="Gradient Tailwind"
-                    checked={tabGradient === 'tailwind'}
-                    onChange={() => handleChangeTab('tailwind')}
-                />
-            </div>
             <Suspense fallback={<div>Loading...</div>}>
-                <div className="flex flex-col gap-2">
-                    {tabGradient === 'css' && <GradientGenerator />}
-                    {tabGradient === 'tailwind' && <TailwindGradientGenerator />}
-                </div>
+                <GradientComponent params={params} />
             </Suspense>
         </LayoutSubSection>
     );
