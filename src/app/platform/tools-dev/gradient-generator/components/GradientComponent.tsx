c0b154660699b1302cfc85d6d8892e7adb8cc84b
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-05-29 18:05:40 -0600

    refactor: Implement tab switching functionality in GradientComponent

---
+++ src/app/platform/tools-dev/gradient-generator/components/GradientComponent.tsx
@@ -0,0 +1,56 @@
+import { ReadonlyURLSearchParams } from 'next/navigation';
+import { useState, useLayoutEffect } from 'react';
+import { Toaster } from 'react-hot-toast';
+import GradientGenerator from './GradientGenerator';
+import TailwindGradientGenerator from './TailwindGradientGenerator';
+
+interface Props {
+    params: ReadonlyURLSearchParams;
+}
+
+export default function GradientComponent({ params }: Props) {
+    const [tabGradient, setTabGradient] = useState<'css' | 'tailwind'>('css');
+
+    useLayoutEffect(() => {
+        const type = params.get('type');
+        if (type === 'tailwind') {
+            setTabGradient('tailwind');
+        } else {
+            setTabGradient('css');
+        }
+    }, [params]);
+
+    const handleChangeTab = (tab: 'css' | 'tailwind') => {
+        const url = new URL(window.location.href);
+        url.searchParams.set('type', tab);
+        window.history.replaceState(null, '', url);
+    };
+    return (
+        <>
+            <Toaster position="top-center" />
+            <div className="tabs tabs-box">
+                <input
+                    type="radio"
+                    name="tabGradient"
+                    className="tab"
+                    aria-label="Gradient CSS"
+                    checked={tabGradient === 'css'}
+                    onChange={() => handleChangeTab('css')}
+                />
+                <input
+                    type="radio"
+                    name="tabGradient"
+                    className="tab"
+                    aria-label="Gradient Tailwind"
+                    checked={tabGradient === 'tailwind'}
+                    onChange={() => handleChangeTab('tailwind')}
+                />
+            </div>
+
+            <div className="flex flex-col gap-2">
+                {tabGradient === 'css' && <GradientGenerator />}
+                {tabGradient === 'tailwind' && <TailwindGradientGenerator />}
+            </div>
+        </>
+    );
+}
